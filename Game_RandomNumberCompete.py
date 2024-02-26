83632af022ace7fb8a70fffd86b9caa9ebd55542
chenboda01 <chenboda01@gmail.com>
2024-02-26 12:25:28 -0500

    add my game

---
+++ Game_RandomNumberCompete.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#
+# By Le Chen and Chatgpt
+# chenle02@gmail.com / le.chen@auburn.edu
+# Created at Mon Feb 26 10:59:43 AM EST 2024
+#
+
+from random import randint
+from time import sleep
+
+boda = randint(1, 100)
+daddy = randint(1, 100)
+
+print("Are you ready to play a game?")
+input = input("Press Enter to continue...")
+
+print("Boda is drawing a number now....")
+sleep(2)
+
+print("Daddy is drawing a number now...")
+sleep(2)
+
+if boda > daddy:
+    print("Boda wins! because boda is", boda, "and daddy is", daddy)
+elif boda < daddy:
+    print("Daddy wins! because boda is", boda, "and daddy is", daddy)
+else:
+    print("It's a tie! because boda is", boda, "and daddy is", daddy)
